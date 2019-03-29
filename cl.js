85b15f39b9822cb2d8ecf41d7f0d0ae8a34ce6db
beyondlimits <beyondlimits@10g.pl>
2019-03-29 08:20:01 +0100

    Almost initial commit

---
+++ cl.js
@@ -0,0 +1,197 @@
+'use strict';
+
+var CL = function(undefined){
+	var isArray = Array.isArray;
+	var splice = Array.prototype.splice;
+
+	// Copy term recursively.
+	function copy(a) {
+		return isArray(a) ? a.map(copy) : a;
+	}
+
+	// Rewrite term using rule. Always drops the first subterm.
+	//   Example: a = ['K', 'a', 'b', 'c'], from = 'xy', to = 'x'
+	//   a becumes ['a', 'c'] after rewrite.
+	function rewrite(a, from, to) {
+		var n = from.length;
+
+		if (n >= a.length) {
+			// pattern is too large and won't match
+			return false;
+		}
+
+
+		var captured = {}; // variables matched with subterms
+		var used = {};     // to avoid referencing array more than one time
+
+		for (var i = 0; i < n; ++i) {
+			var c = from[i];
+
+			if (captured.hasOwnProperty(c)) {
+				throw new TypeException('Pattern ' + from + ' contains repeated characters.');
+			}
+
+			captured[c] = a[i+1];
+		}
+
+		var b = [0, n + 1]; // prepopulated for splice.apply
+		var stack = [];
+		var current = b;
+
+		n = to.length;
+
+		for (var i = 0; i < n; ++i) {
+			var c = to[i];
+
+			switch (c) {
+				case '(':
+					stack.push(current);
+					current = [];
+					break;
+
+				case ')':
+					if (!stack.length) {
+						throw new TypeError('Unbalanced parentheses');
+					}
+
+					var temp = stack.pop();
+					temp.push(current);
+					current = temp;
+					break;
+
+				case ' ':
+					break;
+
+				default:
+					if (used.hasOwnProperty(c)) {
+						// this array was already used - must be copied
+						current.push(copy(captured[c]));
+					} else if (captured.hasOwnProperty(c)) {
+						current.push(captured[c]);
+						used[c] = undefined;
+					} else {
+						// unknown combinator, leave as is.
+						current.push(c);
+					}
+			}
+		}
+
+		if (current != b) {
+			throw new TypeError('Unbalanced parentheses');
+		}
+
+		splice.apply(a, b);
+
+		return true;
+	}
+
+	// Parses the term (string) into equivalent array representation.
+	// Example: 'S(KS)K' is converted to ['S', ['K', 'S'], 'K'].
+	function parse(s) {
+		if (typeof s != 'string') {
+			throw new TypeError('Term must be a string');
+		}
+
+		var result = [];
+		var stack = [];
+		var current = result;
+
+		for (var i = 0, n = s.length; i < n; ++i) {
+			switch (s[i]) {
+				case '(':
+					stack.push(current);
+					current = [];
+					break;
+
+				case ')':
+					if (!stack.length) {
+						throw new TypeError('Unbalanced parentheses');
+					}
+
+					var temp = stack.pop();
+					temp.push(current);
+					current = temp;
+					break;
+
+				case ' ':
+					break;
+
+				default:
+					current.push(s[i]);
+			}
+		}
+
+		if (current != result) {
+			throw new TypeError('Unbalanced parentheses');
+		}
+
+		return result;
+	}
+
+	// Converts the term (array) to string.
+	// Example: ['S', ['K', 'S'], 'K'] is converted to 'S(KS)K'.
+	function stringify(a) {
+		var result = '';
+
+		for (var i = 0, n = a.length; i < n; ++i) {
+			result += isArray(a[i]) ? '(' + stringify(a[i]) + ')' : a[i];
+		}
+
+		return result;
+	}
+
+	// Drops parentheses from the first subterm.
+	// Example: Given a = [['S', 'K'], ['K', 'K']],
+	// a becomes ['S', 'K', ['K', 'K']] after drop.
+	function dropParentheses(a) {
+		while (isArray(a[0])) {
+			var b = [0, 1].concat(a[0]);
+			splice.apply(a, b);
+		}
+	}
+
+	// Evaluate the term using rules.
+	function evaluate(a, rules, count) {
+		dropParentheses(a);
+
+		if (typeof count == 'undefined') {
+			count = 1 / 0;
+		}
+
+		var result = 0;
+
+		for (var i = 0, n = a.length; i < n && result < count; ++i) {
+			if (isArray(a[i])) {
+				result += evaluate(a[i], rules, count - result);
+
+				// to drop parentheses around single combinators
+				if (a[i].length == 1) {
+					a[i] = a[i][0];
+				}
+			}
+		}
+
+		if (count > 0) {
+			if (!isArray(a[0]) && rules.hasOwnProperty(a[0])) {
+				var rule = rules[a[0]];
+				if (rewrite(a, rule[0], rule[1])) {
+					++result;
+				}
+			}
+		}
+
+		//dropParentheses(a);
+
+		return result;
+	}
+
+	return {
+		copy: copy,
+		dropParentheses: dropParentheses,
+		evaluate: evaluate,
+		parse: parse,
+		rewrite: rewrite,
+		stringify: stringify
+	}
+}();
+
---
+++ script.js
@@ -0,0 +1,128 @@
+'use strict';
+
+!function(){
+	var evaluationArea = document.getElementById('evaluationArea');
+	var form = document.getElementById('form');
+	var input = document.getElementById('input');
+	var stepTemplate = document.getElementById('stepTemplate');
+
+	var rulePriority = [
+		// produce less terms than consume
+		'I', 'K',
+
+		// only swap terms around
+		'B', 'C', 'T', 'V',
+
+		// produce little more terms than consume
+		'ι',
+
+		// possibly explosive (may produce a lot of terms)
+		'S', 'W', 'Y', 'U', 'M', 'ω', 'Ω'
+	];
+
+	// rules of substitution
+	var rules = {
+		I: ['x', 'x'],         // Identity
+		K: ['xy', 'x'],        // Kestrel
+		S: ['xyz', 'xz(yz)'],  // Starling
+		ι: ['x', 'xSK'],       // Iota
+		B: ['xyz', 'x(yz)'],   // Bluebird
+		C: ['xyz', 'xzy'],     // Cardinal
+		T: ['xy', 'yx'],       // Thrush
+		V: ['xyz', 'zxy'],     // Vireo
+		W: ['xy', 'xyy'],      // Warbler
+		Y: ['x', 'x(Yx)'],     // Sage
+		U: ['xy', 'y(xxy)'],   // Turing
+		M: ['x', 'xx'],        // Mockingbird
+		ω: ['x', 'xx'],        // alias of Mockingbird
+		Ω: ['', 'ωω']          // Omega
+	};
+
+	// convert rules so that we can evaluate them with priority
+	for (var k in rules) {
+		var r = {}
+		r[k] = rules[k];
+		rules[k] = r;
+	}
+
+	stepTemplate.remove();
+	stepTemplate.removeAttribute('id');
+	stepTemplate.classList.remove('template');
+
+	form.addEventListener('input', evaluate);
+
+	function evaluate() {
+		evaluationArea.innerHTML = '';
+
+		try {
+			var term = CL.parse(input.value);
+		} catch (e) {
+			addStep(e.message, 'error');
+			return;
+		}
+
+		var s = CL.stringify(term);
+		var nodes = {};
+		nodes[s] = addStep(s);
+
+		for(var i = 0; i < 999; i++) {
+			var sOld = s;
+			try {
+				var rule = evaluateRules(term);
+				s = CL.stringify(term);
+			} catch (e) {
+				addStep(e.message, 'error');
+				break;
+			}
+
+			if (s == sOld) {
+				break;
+			}
+
+			var node = addStep(s);
+
+			if (nodes[s]) {
+				nodes[s].classList.add('repetition');
+				node.classList.add('repetition');
+				break;
+			}
+
+			if (s.length > 10000) {
+				// term became way too long
+				addStep('*** BOOM! ***', 'error');
+				break;
+			}
+
+			nodes[s] = node;
+		}
+	}
+
+	function evaluateRules(term) {
+		for (var i = 0, n = rulePriority.length; i < n; ++i) {
+			var k = rulePriority[i];
+			if (CL.evaluate(term, rules[k], 1)) {
+				return k;
+			}
+		}
+	}
+
+	function addStep(s, classes) {
+		var node = stepTemplate.cloneNode();
+
+		node.innerText = s;
+
+		if (classes) {
+			if (typeof classes == 'string') {
+				node.classList.add(classes);
+			} else if (classes instanceof Array) {
+				for (var i = 0, n = c.length; i < n; i++) {
+					node.classList.add(classes[i]);
+				}
+			}
+		}
+
+		evaluationArea.appendChild(node);
+
+		return node;
+	}
+}();
